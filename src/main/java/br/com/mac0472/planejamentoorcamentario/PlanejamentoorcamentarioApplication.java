d2b07b2071407935221b8e2e99948277a9746edc
larissavitoriamedeiros@usp.br <larissavitoriamedeiros@usp.br>
2023-09-16 16:00:23 -0300

    first commit

---
+++ src/main/java/br/com/mac0472/planejamentoorcamentario/PlanejamentoorcamentarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.mac0472.planejamentoorcamentario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanejamentorcamentarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanejamentoorcamentarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mac0472/planejamentoorcamentario/PlanejamentoorcamentarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mac0472.planejamentoorcamentario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanejamentoorcamentarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
