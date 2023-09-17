ed70a6b9e74c886d5d1cc8808835a8a15c1cd0b2
larissavitoriamedeiros@usp.br <larissavitoriamedeiros@usp.br>
2023-09-16 23:05:28 -0300

    Configurate database connection, local authentication and migrations

--- src/main/java/br/com/mac0472/planejamentoorcamentario/PlanejamentoorcamentarioApplication.java
+++ src/main/java/br/com/mac0472/planejamentoorcamentario/PlanejamentoorcamentarioApplication.java
@@ -2,12 +2,13 @@ package br.com.mac0472.planejamentoorcamentario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
-public class PlanejamentorcamentarioApplication {
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // test local without authenticating
+public class PlanejamentoOrcamentarioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PlanejamentoorcamentarioApplication.class, args);
+		SpringApplication.run(PlanejamentoOrcamentarioApplication.class, args);
 	}
 
 }
