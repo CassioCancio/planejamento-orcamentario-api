5f700635df407f3e68101f7932fcf72a0f35ad77
Pedro Lucas R S Campos <plucasrsc@gmail.com>
2023-09-20 15:54:53 -0300

    Add Profile and its getAll method

---
+++ src/main/java/br/com/mac0472/planejamentoorcamentario/controller/ProfileController.java
@@ -0,0 +1,25 @@
+package br.com.mac0472.planejamentoorcamentario.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mac0472.planejamentoorcamentario.entity.Profile;
+import br.com.mac0472.planejamentoorcamentario.repository.ProfileRepository;
+
+@RestController
+@RequestMapping("/profile")
+public class ProfileController {
+
+	@Autowired
+	private ProfileRepository repository;
+	
+	@GetMapping
+	public List<Profile>  getAllProfiles() {
+		return repository.findAll();
+	}
+
+}
---
+++ src/main/java/br/com/mac0472/planejamentoorcamentario/repository/ProfileRepository.java
@@ -0,0 +1,9 @@
+package br.com.mac0472.planejamentoorcamentario.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.mac0472.planejamentoorcamentario.entity.Profile;
+
+public interface ProfileRepository extends JpaRepository<Profile, Long>{
+
+}
