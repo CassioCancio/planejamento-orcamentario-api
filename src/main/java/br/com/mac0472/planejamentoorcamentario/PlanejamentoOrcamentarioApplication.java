f66fa5c961093e25597f593cf60f7b90b1332327
jvml-pessoal <133191177+jvml-pessoal@users.noreply.github.com>
2023-11-27 13:58:50 -0300

    Add CategoryServiceTest and UserServiceTest

--- src/main/java/br/com/mac0472/planejamentoorcamentario/PlanejamentoOrcamentarioApplication.java
+++ src/main/java/br/com/mac0472/planejamentoorcamentario/PlanejamentoOrcamentarioApplication.java
@@ -3,6 +3,7 @@ package br.com.mac0472.planejamentoorcamentario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // test local without authenticating
 public class PlanejamentoOrcamentarioApplication {
